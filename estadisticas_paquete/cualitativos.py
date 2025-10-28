c32367559c2b8d2919a328207e5d6004ad43b842
palmacruzyasmin-oss <palmacruzyasmin@gmail.com>
2025-10-28 10:39:57 -0500

    agregando constructor y atributos básicos

--- estadisticas_paquete/cualitativos.py
+++ estadisticas_paquete/cualitativos.py
@@ -1,3 +1,15 @@
 class Cualitativos:
     """Clase para estadísticas cualitativas (esqueleto inicial)."""
     pass
+class Cualitativos:
+    """Clase para estadísticas cualitativas."""
+
+    def __init__(self, datos=None, nombre="Variable_Cualitativa"):
+        self.nombre = nombre
+        self.datos = []
+        if datos is not None:
+            for x in datos:
+                self.datos.append(x)
+
+        self._tabla = None
+        self._modas = None
