1d945d4a3b60c1bf3e2babdf97a9a71559e23379
palmacruzyasmin-oss <palmacruzyasmin@gmail.com>
2025-10-28 10:47:57 -0500

    implementando frecuencias (diccionario {valor: conteo])

--- estadisticas_paquete/cualitativos.py
+++ estadisticas_paquete/cualitativos.py
@@ -34,3 +34,35 @@ class Cualitativos:
 
     def _contar_elementos(self):
         return self._longitud(self.datos)
+
+class Cualitativos:
+    """Clase para estadísticas cualitativas."""
+
+    def __init__(self, datos=None, nombre="Variable_Cualitativa"):
+        self.nombre = nombre
+        self.datos = []
+        if datos is not None:
+            for x in datos:
+                self.datos.append(x)
+        self._tabla = None
+        self._modas = None
+
+    def _longitud(self, iterable):
+        c = 0
+        for _ in iterable:
+            c = c + 1
+        return c
+
+    def _contar_elementos(self):
+        return self._longitud(self.datos)
+
+    def _frecuencias(self):
+        """Devuelve diccionario {valor: conteo}."""
+        frec = {}
+        for valor in self.datos:
+            if valor in frec:
+                frec[valor] = frec[valor] + 1
+            else:
+                frec[valor] = 1
+        return frec
+
