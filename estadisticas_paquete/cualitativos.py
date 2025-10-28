a62946920cd2c88f3422309cfba4dfc27e9a8564
palmacruzyasmin-oss <palmacruzyasmin@gmail.com>
2025-10-28 10:44:35 -0500

    implementando conteo de elementos

--- estadisticas_paquete/cualitativos.py
+++ estadisticas_paquete/cualitativos.py
@@ -13,3 +13,24 @@ class Cualitativos:
 
         self._tabla = None
         self._modas = None
+
+class Cualitativos:
+    """Clase para estadísticas cualitativas."""
+
+    def __init__(self, datos=None, nombre="Variable_Cualitativa"):
+        self.nombre = nombre
+        self.datos = []
+        if datos is not None:
+            for x in datos:
+                self.datos.append(x)
+        self._tabla = None
+        self._modas = None
+
+    def _longitud(self, iterable):
+        c = 0
+        for _ in iterable:
+            c = c + 1
+        return c
+
+    def _contar_elementos(self):
+        return self._longitud(self.datos)
